f7115db520ec6db8008384d96b742718af8ce7c7
hikalium <hikalium@hikalium.com>
2020-12-13 18:31:39 +0900

    update

--- adventofcode2020/day5_1.rs
+++ adventofcode2020/day5_1.rs
@@ -1,4 +1,5 @@
 use std::cmp;
+use std::convert::TryFrom;
 use std::io::{self, Read};
 
 #[cfg(test)]
@@ -20,6 +21,7 @@ fn main() -> io::Result<()> {
     io::stdin().read_to_string(&mut buffer)?;
     let entries: Vec<&str> = buffer.split('\n').collect();
     let mut max_key = 0;
+    let mut sheets = vec![vec![false; 8]; 128];
     for e in entries {
         if e.len() == 0 {
             continue;
@@ -28,10 +30,32 @@ fn main() -> io::Result<()> {
         let col_key = &e[7..10].replace("L", "0").replace("R", "1");
         let key_str = row_key.to_string() + col_key;
         let key = isize::from_str_radix(&key_str, 2).unwrap();
+        let y = usize::try_from(key / 8).unwrap();
+        let x = usize::try_from(key % 8).unwrap();
 
         println!("{} {} {} {} {}", e, row_key, col_key, key_str, key);
         max_key = cmp::max(max_key, key);
+        sheets[y][x] = true;
+    }
+    let mut has_non_empty_sheet_found = false;
+    let mut has_sheet_found = false;
+    let mut my_sheet_id = 0;
+    let mut sheet_id = 0;
+    for row in sheets {
+        for sheet in row {
+            print!("{} ", sheet);
+            if !has_non_empty_sheet_found && sheet {
+                has_non_empty_sheet_found = true;
+            }
+            if has_non_empty_sheet_found && !has_sheet_found && !sheet {
+                my_sheet_id = sheet_id;
+                has_sheet_found = true;
+            }
+            sheet_id = sheet_id + 1;
+        }
+        println!("");
     }
     println!("{} is the max key", max_key);
+    println!("{} is my sheet", my_sheet_id);
     Ok(())
 }
